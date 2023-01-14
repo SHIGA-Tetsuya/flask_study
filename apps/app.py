97bfe768bb9dbcecb76863892fd79a7be67527d4
SHIGA-Tetsuya <luckyjapon@gmail.com>
2023-01-14 22:20:21 +0900

    CRUD First Step

---
+++ apps/app.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# instantiation of SQLAlchemy
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    uri = f"sqlite:///{Path(__file__).parent.parent/'local.sqlite'}"
+    # configuration of app
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=uri,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+
+    db.init_app(app)
+    Migrate(app, db)
+    from apps.crud import views as crud_views
+
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    return app
