dbdd5d0f02ae2f61937bf6608f7ad6dab5160a37
SHIGA-Tetsuya <luckyjapon@gmail.com>
2023-01-25 23:42:05 +0900

    crud implements

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,9 @@
-from pathlib import Path
-
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+
+from apps.config import config
 
 # instantiation of SQLAlchemy
 db = SQLAlchemy()
@@ -11,17 +11,10 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
-    uri = f"sqlite:///{Path(__file__).parent.parent/'local.sqlite'}"
     # configuration of app
-    app.config.from_mapping(
-        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI=uri,
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
-    )
+    app.config.from_object(config[config_key])
 
     db.init_app(app)
     Migrate(app, db)
