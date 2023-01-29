828a6578a14fa087ae50b06b3214c5a94268ea4c
SHIGA-Tetsuya <luckyjapon@gmail.com>
2023-01-29 17:39:43 +0900

    login requirement

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import login_user
+
+from apps.app import db
+from apps.auth.forms import SignUpForm
+from apps.crud.models import User
 
 auth = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
 
@@ -6,3 +11,27 @@ auth = Blueprint("auth", __name__, template_folder="templates", static_folder="s
 @auth.route("/")
 def index():
     return render_template("auth/index.html")
+
+
+@auth.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        if user.is_duplicate_email():
+            flash("指定のメールアドレスは登録済みです ")
+            return redirect(url_for("auth.signup"))
+
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        # GETパラメータにnextキーが存在し、値がない場合はユーザーの一覧ページへリダイレクトする
+        next_ = request.args.get("next")
+        if next_ is None or not next_.startswith("/"):
+            next_ = url_for("crud.users")
+        return redirect(next_)
+    return render_template("auth/signup.html", form=form)
