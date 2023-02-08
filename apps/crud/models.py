af34c418069be063b8ee9414ba6718e1e6243962
SHIGA-Tetsuya <luckyjapon@gmail.com>
2023-02-08 19:32:21 +0900

    login feature reuse

--- apps/crud/models.py
+++ apps/crud/models.py
@@ -16,6 +16,10 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
+    user_images = db.relationship(
+        "UserImage", backref="user", order_by="desc(UserImage.id)"
+    )
+
     @property
     def password(self):
         raise AttributeError("読み取り不可")
